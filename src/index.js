e0d768f71b7eeb3f7e18ac5eefbf479eae310f0d
martharoman <118926638+martharoman@users.noreply.github.com>
2023-01-13 14:38:09 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Mi primera practica con Condesandbox</h1>
+<h2>Martha Andrea Roman Hernández</h2>
+<div>
+  Universidad Tecnológica de Durango
+  <a href="https://github.com/martharoman/proyecto_react_isaac" target="_blank" rel="noopener noreferrer">Mi GitHub</a>.
+</div>
+`;
